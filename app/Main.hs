4374eb4ab987bb515d55fdfdf625860240e26fd7
acb <acb@staticplain.local>
2018-05-09 18:39:31 +0100

    Tabs -> spaces, and minor refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -49,56 +49,56 @@ commandLineParser = CommandLineOptions
 -- JSON parsing
 
 data ArtistRecord = ArtistRecord {
-	name :: String,
-	playcount :: Int
+    name :: String,
+    playcount :: Int
 } deriving (Show)
 
 --data TopArtistsResponse = 
 
 --topArtistsResponse :: Value -> Parser [ArtistRecord]
 topArtistsResponse = withObject "topartists" $ \o -> do
-	p <- o .: "topartists"
-	artists <- p .: "artist"
-	return artists :: Parser [ArtistRecord]
+    p <- o .: "topartists"
+    artists <- p .: "artist"
+    return artists :: Parser [ArtistRecord]
 
 instance FromJSON ArtistRecord where
-	--parseJSON = withObject "artistRecord" $ \o -> ArtistRecord <$> o .: "name" <*> (readMaybe (o .: "playcount")) :: Int
-	parseJSON = withObject "artistRecord" $ \o -> do
-		name <- o .: "name"
-		countStr <- o .: "playcount"
-		case readMaybe countStr :: Maybe Int of
-			Just c -> return $ ArtistRecord name c
-			Nothing -> fail "No well-formed playcount"
+    --parseJSON = withObject "artistRecord" $ \o -> ArtistRecord <$> o .: "name" <*> (readMaybe (o .: "playcount")) :: Int
+    parseJSON = withObject "artistRecord" $ \o -> do
+        name <- o .: "name"
+        countStr <- o .: "playcount"
+        case readMaybe countStr :: Maybe Int of
+            Just c -> return $ ArtistRecord name c
+            Nothing -> fail "No well-formed playcount"
 --
 
 makeReport :: String -> [ArtistRecord] -> String
 makeReport banner topArtists = banner ++ body
-	where
-		body = foldr1 (\a b -> a++", "++b) $ fmap asString topArtists
-		asString (ArtistRecord name count) = name ++ " (" ++ (show count) ++ ")"
+    where
+        body = foldr1 (\a b -> a++", "++b) $ fmap asString topArtists
+        asString (ArtistRecord name count) = name ++ " (" ++ (show count) ++ ")"
 
 
 
 urlDataToReport :: B.ByteString -> Maybe String
 urlDataToReport j = do
-	d <- decode j
-	ar <- parseMaybe topArtistsResponse d
-	return $ makeReport "Top last.fm artists of the past week: " ar
+    d <- decode j
+    ar <- parseMaybe topArtistsResponse d
+    return $ makeReport "Top last.fm artists of the past week: " ar
 
 
 --  an attempt to reimplement main with maybeT
 
 data ReportSpec = TopArtistsReport LastFM.TopArtistsPeriod Int
 
+specToRequest :: Config.Config -> ReportSpec -> LastFM.Request
+specToRequest config (TopArtistsReport p l) = LastFM.UserTopArtists (Config.lastUsername config) p l
+
 getReport :: ReportSpec -> MaybeT IO String
 getReport spec = do
-
-	config <- MaybeT $ Config.getConfig
-	let request = case spec of 
-		TopArtistsReport period limit -> LastFM.UserTopArtists (Config.lastUsername config) period limit
-	let url = LastFM.requestURL (Config.lastAPIKey config) request
-	j <- liftIO $ simpleHttp url
-	MaybeT $ return $ urlDataToReport j
+    config <- MaybeT $ Config.getConfig
+    let url = LastFM.requestURL (Config.lastAPIKey config) $ specToRequest config spec
+    j <- liftIO $ simpleHttp url
+    MaybeT $ return $ urlDataToReport j
 
 -- 
 
--- src/LastSum/Config.hs
+++ src/LastSum/Config.hs
@@ -15,25 +15,25 @@ configName = fmap (\d -> d ++ "/config") $ getAppUserDataDirectory appName
 
 --readConfigFile :: MonadError CF.CPError m => IO (m CF.ConfigParser) 
 --readConfigFile = do 
---	dataDir <- getAppUserDataDirectory appName
---	return $ CF.readfile CF.emptyCP $ dataDir ++ "/config"
+--  dataDir <- getAppUserDataDirectory appName
+--  return $ CF.readfile CF.emptyCP $ dataDir ++ "/config"
 
 
 -- TODO: make this return an Either
 --getConfig :: Error e => IO (Either e (String, String))
 getConfig :: IO (Maybe Config)
 getConfig = do
-	configFileName <- configName
+    configFileName <- configName
 
-	rv <- runErrorT $
-		do
-			cp <- join $ liftIO $ CF.readfile CF.emptyCP configFileName
-			let x = cp
-			--return $ (\a b -> (a,b)) <$> (CF.get x "DEFAULT" "username") <*> (CF.get x "DEFAULT" "api_key")
- 			username <- CF.get x "DEFAULT" "username"
-			apiKey <- CF.get x "DEFAULT" "api_key"
-			return $ Config username apiKey
-	--return rv
-	return $ case rv of
-		Left _ -> Nothing
-		Right cfg -> Just cfg
+    rv <- runErrorT $
+        do
+            cp <- join $ liftIO $ CF.readfile CF.emptyCP configFileName
+            let x = cp
+            --return $ (\a b -> (a,b)) <$> (CF.get x "DEFAULT" "username") <*> (CF.get x "DEFAULT" "api_key")
+            username <- CF.get x "DEFAULT" "username"
+            apiKey <- CF.get x "DEFAULT" "api_key"
+            return $ Config username apiKey
+    --return rv
+    return $ case rv of
+        Left _ -> Nothing
+        Right cfg -> Just cfg
