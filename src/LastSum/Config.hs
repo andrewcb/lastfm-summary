626403c20bd2eac7d6110dbcb106068d2d2a1fd5
acb <acb@staticplain.local>
2018-05-11 16:49:18 +0100

    getMaybe implemented

--- src/LastSum/Config.hs
+++ src/LastSum/Config.hs
@@ -3,7 +3,9 @@ where
 
 import System.Directory
 import qualified Data.ConfigFile as CF
+import qualified Data.ConfigFile.Types as CFT
 import Control.Monad.Error
+import Control.Monad.Except
 
 data Config = Config { lastUsername :: String, lastAPIKey :: String  }
 
@@ -19,6 +21,14 @@ configName = fmap (\d -> d ++ "/config") $ getAppUserDataDirectory appName
 --  return $ CF.readfile CF.emptyCP $ dataDir ++ "/config"
 
 
+
+getMaybe :: (CF.Get_C a) => CF.ConfigParser -> CF.SectionSpec -> CF.OptionSpec -> Maybe a
+getMaybe cp sect opt = do
+        case (runExcept $ CF.get cp sect opt) of
+            Right x -> Just x
+            Left _ -> Nothing
+
+
 -- TODO: make this return an Either
 --getConfig :: Error e => IO (Either e (String, String))
 getConfig :: IO (Maybe Config)
@@ -32,8 +42,12 @@ getConfig = do
             --return $ (\a b -> (a,b)) <$> (CF.get x "DEFAULT" "username") <*> (CF.get x "DEFAULT" "api_key")
             username <- CF.get x "DEFAULT" "username"
             apiKey <- CF.get x "DEFAULT" "api_key"
+            --let blah = (getMaybe x "DEFAULT" "blah") :: Maybe String
+            --liftIO $ putStrLn $ "blah = " ++ (show blah)
+
             return $ Config username apiKey
     --return rv
     return $ case rv of
         Left _ -> Nothing
         Right cfg -> Just cfg
+
