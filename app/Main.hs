f6c1a0af4706713a36ea5954287f4da7fce95b57
acb <acb@staticplain.local>
2018-05-12 18:27:30 +0100

    Moved reporting code to Reporting module

--- app/Main.hs
+++ app/Main.hs
@@ -5,49 +5,16 @@ module Main where
 import qualified LastSum.Lib.API.LastFM as LastFM
 import LastSum.Lib.API.LastFM.Parsing (ArtistRecord, pattern ArtistRecord, parseArtistsResponse)
 import qualified LastSum.Settings as Settings
+import qualified LastSum.Reporting as Reporting
 
-import Network.HTTP.Conduit (simpleHttp)
-import qualified Data.ByteString.Lazy as B
-import Data.Either.Utils
 import Data.String.Utils (replace)
 import Control.Monad.Except
 import Control.Monad.Trans.Maybe
 import Text.Read (readMaybe)
 
 
-makeTopArtistsReport :: String -> [ArtistRecord] -> String
-makeTopArtistsReport banner topArtists = banner ++ body
-    where
-        body = foldr1 (\a b -> a++", "++b) $ fmap asString topArtists
-        asString (ArtistRecord name count) = name ++ " (" ++ (show count) ++ ")"
-
-artistsURLDataToReport :: String -> B.ByteString -> Maybe String
-artistsURLDataToReport banner j = do
-    ar <- parseArtistsResponse j
-    return $ makeTopArtistsReport banner ar
-
-
---  an attempt to reimplement main with maybeT
-
-data ReportSpec = TopArtistsReport String LastFM.RetroPeriod Int
-
-specFromSettings :: Settings.Settings -> ReportSpec
-specFromSettings settings = TopArtistsReport (Settings.lastUsername settings) (Settings.reportPeriod settings) (Settings.reportLimit settings)
-
-specToRequest :: ReportSpec -> LastFM.Request
-specToRequest (TopArtistsReport u p l) = LastFM.UserTopArtists u p l
-
-specToBanner :: ReportSpec -> String
-specToBanner (TopArtistsReport u p l) = "Top last.fm artists of " ++ LastFM.descriptiveName p ++ ": "
-
-getReport :: Settings.Settings -> ReportSpec -> MaybeT IO String
-getReport settings spec = do
-    --config <- MaybeT $ Config.getConfig
-    let url = LastFM.requestURL (Settings.lastAPIKey settings) $ specToRequest spec
-    j <- liftIO $ simpleHttp url
-    MaybeT $ return $ artistsURLDataToReport (specToBanner spec) j
-
--- 
+specFromSettings :: Settings.Settings -> Reporting.ReportSpec
+specFromSettings settings = Reporting.TopArtistsReport (Settings.lastUsername settings) (Settings.reportPeriod settings) (Settings.reportLimit settings)
 
 main :: IO ()
 main = do
@@ -55,5 +22,5 @@ main = do
     case s of 
         Left err -> putStrLn err
         Right settings -> do
-            r <- runMaybeT $ getReport settings (specFromSettings settings)
+            r <- runMaybeT $ Reporting.getReport settings (specFromSettings settings)
             putStrLn $ maybe "Nothing to report" id r
---
+++ src/LastSum/Reporting.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings, PatternSynonyms #-}
+
+module LastSum.Reporting
+where
+
+import qualified LastSum.Settings as Settings
+import qualified LastSum.Lib.API.LastFM as LastFM
+import LastSum.Lib.API.LastFM.Parsing (ArtistRecord, pattern ArtistRecord, parseArtistsResponse)
+import Network.HTTP.Conduit (simpleHttp)
+import qualified Data.ByteString.Lazy as B
+import Data.Either.Utils
+import Control.Monad.Except
+import Control.Monad.Trans.Maybe
+
+
+data ReportSpec = TopArtistsReport String LastFM.RetroPeriod Int
+
+makeTopArtistsReport :: String -> [ArtistRecord] -> String
+makeTopArtistsReport banner topArtists = banner ++ body
+    where
+        body = foldr1 (\a b -> a++", "++b) $ fmap asString topArtists
+        asString (ArtistRecord name count) = name ++ " (" ++ (show count) ++ ")"
+
+artistsURLDataToReport :: String -> B.ByteString -> Maybe String
+artistsURLDataToReport banner j = do
+    ar <- parseArtistsResponse j
+    return $ makeTopArtistsReport banner ar
+
+
+
+specToRequest :: ReportSpec -> LastFM.Request
+specToRequest (TopArtistsReport u p l) = LastFM.UserTopArtists u p l
+
+specToBanner :: ReportSpec -> String
+specToBanner (TopArtistsReport u p l) = "Top last.fm artists of " ++ LastFM.descriptiveName p ++ ": "
+
+getReport :: Settings.Settings -> ReportSpec -> MaybeT IO String
+getReport settings spec = do
+    --config <- MaybeT $ Config.getConfig
+    let url = LastFM.requestURL (Settings.lastAPIKey settings) $ specToRequest spec
+    j <- liftIO $ simpleHttp url
+    MaybeT $ return $ artistsURLDataToReport (specToBanner spec) j
