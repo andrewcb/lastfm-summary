c6941ad8d3e306375979eaf5a6ed7353ed015fa3
acb <acb@staticplain.local>
2018-05-14 01:22:48 +0100

    subject is now a required positional argument; other improvements to command-line help

--- src/LastSum/Settings/CommandLine.hs
+++ src/LastSum/Settings/CommandLine.hs
@@ -9,9 +9,9 @@ import Data.Semigroup ((<>))
 
 data CommandLineOptions = CommandLineOptions {
     lastUsername :: Maybe String,
-    subject :: ReportSubject,
     period :: LastFM.RetroPeriod,
-    limit :: Int
+    limit :: Int,
+    subject :: ReportSubject
 } deriving Show
 
 validSubjects = foldr1 (\a b -> a++", "++b) reportSubjectNames
@@ -21,30 +21,27 @@ validPeriods = foldr1 (\a b -> a++", "++b)  LastFM.retroPeriodNames
 parseSubject :: OPT.ReadM ReportSubject
 parseSubject = OPT.eitherReader $ \s -> case parseReportSubject s of
     Just s -> Right s
-    Nothing -> Left $ "Invalid subject specified; should be one of " ++ validSubjects
+    Nothing -> Left $ "Invalid subject specified; should be one of: " ++ validSubjects
 
 parsePeriod :: OPT.ReadM LastFM.RetroPeriod
 parsePeriod = OPT.eitherReader $ \s -> case LastFM.parseRetroPeriod s of
     Just p -> Right p
-    Nothing -> Left $ "Invalid period specified; should be one of " ++ validPeriods
+    Nothing -> Left $ "Invalid period specified; should be one of: " ++ validPeriods
 
 commandLineParser :: OPT.Parser CommandLineOptions
 commandLineParser = CommandLineOptions
     <$> OPT.option (OPT.maybeReader (\s -> Just $ Just s)) (
         OPT.long "last-username"
         <> OPT.short 'u'
-        <> OPT.help "last.fm username to examine"
+        <> OPT.metavar "NAME"
+        <> OPT.help "the username of the last.fm feed to examine"
         <> OPT.value Nothing
         )
-    <*> OPT.option parseSubject (
-        OPT.long "what"
-        <> OPT.short 'w'
-        <> OPT.help ("the data to report; one of " ++ validSubjects)
-        <> OPT.value RecentTracks )
     <*> OPT.option parsePeriod (
         OPT.long "period" 
         <> OPT.short 'p' 
-        <> OPT.help ("period of report; one of " ++ validPeriods)
+        <> OPT.help ("period of report; one of: " ++ validPeriods)
+        <> OPT.metavar "P"
         <> OPT.value LastFM.P7Day )
     <*> OPT.option OPT.auto (
         OPT.long "limit"
@@ -53,6 +50,8 @@ commandLineParser = CommandLineOptions
         <> OPT.metavar "N"
         <> OPT.value 5
         <> OPT.showDefault )
+    <*> OPT.argument parseSubject (
+        OPT.metavar "subject"  )
 
 
 getOptions :: IO CommandLineOptions
@@ -60,5 +59,5 @@ getOptions = OPT.execParser opts
     where
         opts = OPT.info (commandLineParser OPT.<**> OPT.helper)
             ( OPT.fullDesc 
-                <> OPT.progDesc "produce a report from last.fm user listening data"
+                <> OPT.progDesc ("Produce a report from last.fm user listening data. The subject can be one of: " ++ validSubjects)
                 <> OPT.header "lastfm-summary" )
